da97e1ecccdbcb43cd4be448ed7474f742a6a6fe
AlanTH-cwb <alanthdev@gmail.com>
2025-07-30 07:40:11 -0300

    Add files via upload

---
+++ Primeiro desafio PYTHON.py
@@ -0,0 +1,19 @@
+#Desafio 01
+
+nome=input('Qual é o seu nome? ')
+print('Olá ',nome,'! Prazer em te conhecer!')
+
+#Desafio 02
+
+dia=input('Dia = ')
+mes=input('Mês = ')
+ano=input('Ano = ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+#Desafio 03
+
+a=int(input('Digite o primeiro número: '))
+b=int(input('Digite o segundo número: '))
+soma= a + b
+print('A soma entre os dois números é igual a ',soma,'!')
+
